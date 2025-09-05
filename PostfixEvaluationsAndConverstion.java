4c116af6ad40d740f79676b2d81f91b9fc52f66b
MustafaQadeer <mustafa.qadeer2020@gmail.com>
2025-09-05 21:40:08 +0530

    Update PostfixEvaluationsAndConverstion.java

--- PostfixEvaluationsAndConverstion.java
+++ PostfixEvaluationsAndConverstion.java
@@ -4,6 +4,8 @@ public static void main(String[] args) {
     Scanner scn=new Scanner(System.in);
     System.out.print("Add a no.: ");
     int n=scn.nextInt();
+    
+    //this is a comment
 
 }
 }
