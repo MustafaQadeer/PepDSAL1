79864a92566b5d8e580167876986e1a62d75cf52
MustafaQadeer <mustafa.qadeer2020@gmail.com>
2025-09-06 19:41:44 +0530

    Update PostfixEvaluationsAndConverstion.java

--- PostfixEvaluationsAndConverstion.java
+++ PostfixEvaluationsAndConverstion.java
@@ -2,8 +2,9 @@ import java.util.*;
 public class PostfixEvaluationsAndConverstion {
 public static void main(String[] args) {
     Scanner scn=new Scanner(System.in);
-    System.out.print("Add a no.: ");
+    System.out.print("Enter expression: ");
     int n=scn.nextInt();
+    
     
 
 }
