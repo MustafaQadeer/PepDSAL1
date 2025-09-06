5eedb55bdabafcf11a71182250e965918db78b38
MustafaQadeer <mustafa.qadeer2020@gmail.com>
2025-09-07 03:51:11 +0530

    Finally done!!!!!

--- PostfixEvaluationsAndConverstion.java
+++ PostfixEvaluationsAndConverstion.java
@@ -1,107 +1,52 @@
 import java.util.*;
-
-import javax.print.DocFlavor.STRING;
 public class PostfixEvaluationsAndConverstion {
+public static int operation(int v1,int v2,char ch){
+    if (ch=='+') {
+        return v1+v2;
+    }else if (ch=='-') {
+        return v1-v2;
+    }else if (ch=='*') {
+        return v1*v2;
+    }else{
+        return v1/v2;
+    }
+}
 public static void main(String[] args) {
     Scanner scn=new Scanner(System.in);
     System.out.print("Enter expression: ");
     String exp=scn.nextLine();
 
-    Stack<Integer> vs=new Stack<>();
+    Stack<Integer> vs= new Stack<>();
     Stack<String> is= new Stack<>();
     Stack<String> ps= new Stack<>();
 
     for (int i = 0; i < exp.length(); i++) {
         char ch=exp.charAt(i);
+        if (ch=='+'||ch=='-'||ch=='*'||ch=='/') {
+            // handling vs
+            int vv2=vs.pop();
+            int vv1=vs.pop();
+            int vval=operation(vv1,vv2,ch);
+            vs.push(vval);
+            //handling is
+            String iv2=is.pop();
+            String iv1=is.pop();
+            String ival="("+iv1+ch+iv2+")";
+            is.push(ival);
 
-        if ((ch>='0'&&ch<='9')||(ch>='a'&&ch<='z')||(ch>='A' && ch<='Z')) {
-            vs.push(ch+'0');
+            //handling ps
+            String pv2=ps.pop();
+            String pv1=ps.pop();
+            String pval=ch+pv1+pv2;
+            ps.push(pval);
+        }else{
+            vs.push(ch-'0');
             is.push(ch+"");
             ps.push(ch+"");
         }
-        else if (ch=='+') {
-            //vs handiling
-            int v2vs=vs.pop();
-            int v1vs=vs.pop();
-            int vsval=v1vs+v2vs;
-            vs.push(vsval);
-
-            //is handling
-            String v2is=is.pop();
-            String v1is=is.pop();
-            String isval="("+v1is+"+"+v2is+")";
-            is.push(isval);
-
-            //ps handling
-            String v2ps=ps.pop();
-            String v1ps=ps.pop();
-            String psval="+"+v1ps+v2ps+;
-            ps.push(psval);
-        }else if (ch=='-') {
-            //vs handiling
-            int v2vs=vs.pop();
-            int v1vs=vs.pop();
-            int vsval=v1vs-v2vs;
-            vs.push(vsval);
-
-            //is handling
-            String v2is=is.pop();
-            String v1is=is.pop();
-            String isval="("+v1is+"-"+v2is+")";
-            is.push(isval);
-
-            //ps handling
-            String v2ps=ps.pop();
-            String v1ps=ps.pop();
-            String psval="-"+v1ps+v2ps+;
-            ps.push(psval);
-            
-        }else if (ch=='*') {
-            //vs handiling
-            int v2vs=vs.pop();
-            int v1vs=vs.pop();
-            int vsval=v1vs*v2vs;
-            vs.push(vsval);
-
-            //is handling
-            String v2is=is.pop();
-            String v1is=is.pop();
-            String isval="("+v1is+"*"+v2is+")";
-            is.push(isval);
-
-            //ps handling
-            String v2ps=ps.pop();
-            String v1ps=ps.pop();
-            String psval=v1ps+v2ps+'*';
-            ps.push(psval);
-            
-        }else if (ch=='/') {
-            //vs handiling
-            int v2vs=vs.pop();
-            int v1vs=vs.pop();
-            int vsval=v1vs/v2vs;
-            vs.push(vsval);
-
-            //is handling
-            String v2is=is.pop();
-            String v1is=is.pop();
-            String isval="("+v1is+"/"+v2is+")";
-            is.push(isval);
-
-            //ps handling
-            String v2ps=ps.pop();
-            String v1ps=ps.pop();
-            String psval=v1ps+v2ps+"/";
-            ps.push(psval);
-            
-        }
     }
-    System.out.println(vs.peek());
-    System.out.println(is.peek());
-    System.out.println(ps.peek());
-
-
-    
-
+    System.out.println(vs.pop());
+    System.out.println(is.pop());
+    System.out.println(ps.pop());
 }
 }
