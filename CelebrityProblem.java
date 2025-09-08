0edc493f0556e6a8c815588e26197bde59b28e0f
MustafaQadeer <mustafa.qadeer2020@gmail.com>
2025-09-09 02:32:05 +0530

    CelebrityProblemDone!

---
+++ CelebrityProblem.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class CelebrityProblem {
+
+    public static void FindCelebrity(int[][] arr){
+        Stack<Integer> st=new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            st.push(i);
+        }
+        while (st.size()>=2) {
+            int i=st.pop();
+            int j=st.pop();
+
+            if (arr[i][j]==1) {
+                // if i knows j, i cant be celebrity
+                st.push(j);
+            }else{
+                // if i does not know j, j cant be celebrity
+                st.push(i);
+            }
+        }
+        int pot=st.pop();
+        for (int i = 0; i < arr.length; i++) {
+            if (i!=pot) {
+                if (arr[i][pot]==0||arr[pot][i]==1) {
+                    System.out.println("none");
+                    return;
+                }
+            }
+        }
+        System.out.println(pot);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // Size of array
+
+        //inputing the matrix
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        
+        FindCelebrity(arr);
+    }
+}
